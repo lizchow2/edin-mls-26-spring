38fcea90764032306992a28b2686a7a6b2bdb7a6
Ace0x <andrew.d.72137@gmail.com>
2026-03-10 13:31:00 +0000

    gelu kernel not implemented?

--- hw1-asr/glm_asr_triton_template/layers.py
+++ hw1-asr/glm_asr_triton_template/layers.py
@@ -162,7 +162,7 @@ def gelu_kernel(x_ptr, y_ptr, n_elements, BLOCK_SIZE: tl.constexpr):
     sqrt_2_over_pi = 0.7978845608028654
     x3 = x * x * x
     inner = sqrt_2_over_pi * (x + 0.044715 * x3)
-    y = x * 0.5 * (1.0 + tl.libdevice.tanh(inner))
+    y = x * 0.5 * (1.0 + 2 * tl.sigmoid(2 * inner) - 1)
 
     tl.store(y_ptr + offs, y, mask=mask)
 
@@ -283,7 +283,7 @@ def linear_gelu_kernel(
     sqrt_2_over_pi = 0.7978845608028654
     acc3 = acc * acc * acc
     inner = sqrt_2_over_pi * (acc + 0.044715 * acc3)
-    acc = acc * 0.5 * (1.0 + tl.libdevice.tanh(inner))
+    acc = acc * 0.5 * (1.0 + 2 * tl.sigmoid(2 * inner) - 1)
 
     tl.store(
         c_ptr + offs_m[:, None] * stride_cm + offs_n[None, :] * stride_cn,
